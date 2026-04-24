be8308d084e7aa0160ca2dec06426ec9f1eb47cc
luizpaulo73 <luiz.paulo2005@hotmail.com>
2026-04-24 01:43:55 -0300

    feat: add groups and teams data structure for the album

---
+++ database/albumStructure/groups.ts
@@ -0,0 +1,14 @@
+export const groups = [
+    { group: "A", teams: ["MEX","RSA","KOR","CZE"] },
+    { group: "B", teams: ["CAN","BIH","QAT","SUI"] },
+    { group: "C", teams: ["BRA","MAR","HAI","SCO"] },
+    { group: "D", teams: ["USA","PAR","AUS","TUR"] },
+    { group: "E", teams: ["GER","CUW","CIV","ECU"] },
+    { group: "F", teams: ["NED","JPN","SWE","TUN"] },
+    { group: "G", teams: ["BEL","EGY","IRN","NZL"] },
+    { group: "H", teams: ["ESP","CPV","KSA","URU"] },
+    { group: "I", teams: ["FRA","SEN","IRQ","NOR"] },
+    { group: "J", teams: ["ARG","ALG","AUT","JOR"] },
+    { group: "K", teams: ["POR","COD","UZB","COL"] },
+    { group: "L", teams: ["ENG","CRO","GHA","PAN"] },
+];
